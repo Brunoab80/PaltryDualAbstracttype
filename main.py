0738a0ba619e16eee9b1d35847c7dca83842b6e7
Bruno Bueno <36167391-brunoab80@users.noreply.replit.com>
2024-09-01 13:24:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+d1=random.Random()
+d2=random.Random()
+
+double=0
+rolls= []
+for i in range (10):
+    a=d1.randrange(1,7)
+    b=d2.randrange(1,7)
+    i+=1
+    rolls.append((a,b))
+    print(a,b)
+    if a+b ==12:
+      print ("Duplol Seis!!!")
+      double+=1
+print (rolls)
+print (double)
